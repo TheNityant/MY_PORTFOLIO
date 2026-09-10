fb91b82a68c47350c999ce1f5992ac3d97684a9e
Nityant Tiwari <nityant.tiwari@nldalmia.co.in>
2026-09-10 17:14:15 +0530

    feat: load personalized hero styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./styles/technicalNoir.css";
+import "./styles/heroTechnicalNoir.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
