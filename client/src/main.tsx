ca0582b8827a92efe610d4b7928132613ef27419
Nityant Tiwari <nityant.tiwari@nldalmia.co.in>
2026-09-12 03:19:13 +0530

    fix: load hero visibility overrides

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import "./index.css";
 import "./styles/technicalNoir.css";
 import "./styles/heroTechnicalNoir.css";
+import "./styles/heroVisibilityFix.css";
 import "./styles/siteAtmosphere.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
